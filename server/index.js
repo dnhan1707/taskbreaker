9f1a4636bed5b9f5dba4b31130b6560b51b485e8
Nhan Tri Danh <57444770+dnhan1707@users.noreply.github.com>
2024-10-12 12:29:42 -0700

    third commit

--- server/index.js
+++ server/index.js
@@ -1,8 +1,10 @@
 import express from "express";
 
 
+// const kindoAI = "adeed805-0109-443d-af0e-e92110d48064-6852abcdf3bd2178"
 const app = express();
 const port = 5000;
+// const kindo = new Kindo(apikey)
 
 app.get("/", (req, res) => {
     res.send("Hello")
