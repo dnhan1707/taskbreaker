8c6cccac1c7752e8f65a878d1221a449cd54c549
kylejussab <kylejussab@gmail.com>
2024-10-12 19:43:49 -0700

    preview ui

--- client/src/components/PromptedUser.js
+++ client/src/components/PromptedUser.js
@@ -2,17 +2,36 @@ import React from 'react';
 import styles from './PromptedUser.module.css';
 
 const PromptedUser = ({name, tasks}) => {
+    const calculateDaysBetween = (start, end) => {
+        const startDate = new Date(start);
+        const endDate = new Date(end);
+    
+        const timeDifference = endDate - startDate;
+    
+        const daysDifference = timeDifference / (1000 * 60 * 60 * 24);
+
+        let output;
+
+        if(daysDifference == 1){
+            output = " | " + daysDifference + " day";
+        }
+        else if(daysDifference == 0){
+            output = "";
+        }
+        else{
+            output = " | " + daysDifference + " days";
+        }
+    
+        return output;
+    };
+
     return (
-        <div>
-            <p>{name}</p>
-            
+        <div className={styles.promptedUser}>
+            <p className={styles.name}>{name}</p>
             <ul>
                 {tasks.map((task, index) => (
                     <li key={index} className={styles.task}>
-                        <p><strong>Task:</strong> {task.task}</p>
-                        <p><strong>Start Day:</strong> {task.start_day}</p>
-                        <p><strong>End Day:</strong> {task.end_day}</p>
-                        <p><strong>Need Help:</strong> {task.need_help}</p>
+                        <p>{task.task} {calculateDaysBetween(task.start_day, task.end_day)}</p>
                     </li>
                 ))}
             </ul>
