1e29d2a31f9ce2b9a7e354f45e0026199876c9c3
Hlaing, Khant Nyi <knyihlaing@go.pasadena.edu>
2024-10-12 17:22:51 -0700

    pus

--- client/src/firebase/firebase.js
+++ client/src/firebase/firebase.js
@@ -2,20 +2,14 @@
 import { initializeApp } from "firebase/app";
 import { getAuth } from "firebase/auth";
 
-// import { getAnalytics } from "firebase/analytics";
-// TODO: Add SDKs for Firebase products that you want to use
-// https://firebase.google.com/docs/web/setup#available-libraries
-
-// Your web app's Firebase configuration
-// For Firebase JS SDK v7.20.0 and later, measurementId is optional
 const firebaseConfig = {
   apiKey: process.env.FIREBASE_APIKEY || "AIzaSyBFs9Ypd68JCh0iNf5Sg28auJH2vwaTKe8",
-  authDomain: process.env.FIREBASE_AuthDomain,
-  projectId: process.env.FIREBASE_ProjectId,
-  storageBucket: process.env.FIREBASE_StorageBucket,
-  messagingSenderId: process.env.FIREBASE_MessagingSenderId,
-  appId: process.env.FIREBASE_AppId,
-  measurementId: process.env.FIREBASE_MeasurementId
+  authDomain: process.env.FIREBASE_AUTHDOMAIN,
+  projectId: process.env.FIREBASE_PROJECTID,
+  storageBucket: process.env.FIREBASE_STORAGEBUCKET,
+  messagingSenderId: process.env.FIREBASE_MESSAGINGSENDERID,
+  appId: process.env.FIREBASE_APPID,
+  measurementId: process.env.FIREBASE_MEASUREMENTID,
 };
 
 // Initialize Firebase
