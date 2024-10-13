620a110d0f43be09a8f587f15432bea88b756ff4
kylejussab <kylejussab@gmail.com>
2024-10-12 20:05:23 -0700

    main fixes

--- client/src/components/Dashboard.js
+++ client/src/components/Dashboard.js
@@ -1,5 +1,6 @@
 import React, { useState } from 'react';
 import styles from './Dashboard.module.css';
+import user_prompt from "../prompt.js";
 import PromptedUser from "./PromptedUser.js";
 import DashboardBento from './DashboardBento.js';
 import loadingGif from '../images/loading.gif';
@@ -14,12 +15,23 @@ const Dashboard = () => {
         setLoading(true);
         setShowPreview(true);
         try {
-            const response = await fetch("http://localhost:5000", {
+            const API_KEY = "7a488c20-bf18-47e7-b5b1-79706bf8b572-ce792aa4d58109b9"; // Your API key
+            const BASE_URL = 'https://llm.kindo.ai/v1/chat/completions'; // Kindo API URL
+
+            const prompt = user_prompt(user_prompt_text);
+
+            const data = {
+                model: "azure/gpt-4o",
+                messages: [{ role: "user", content: prompt }],
+            };
+
+            const response = await fetch(BASE_URL, {
                 method: "POST",
                 headers: {
+                    'api-key': API_KEY,
                     'Content-Type': 'application/json',
                 },
-                body: JSON.stringify({prompt: user_prompt_text}), // Stringify the data
+                body: JSON.stringify(data), // Stringify the data
             });
 
             if (!response.ok) {
@@ -27,9 +39,7 @@ const Dashboard = () => {
             }
 
             const responseData = await response.json();
-
-            console.log(responseData.result);
-            setResult(JSON.parse(responseData.result));
+            setResult(JSON.parse(responseData.choices[0].message.content));
             setLoading(false);
         } catch (error) {
             console.error('Error fetching data:', error);
